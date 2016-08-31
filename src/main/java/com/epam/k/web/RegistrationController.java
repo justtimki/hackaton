4cea9ce8a087ffc17a097f448808f469bee408a7
yauhenl <yauhen.lisimau@gmail.com>
2016-08-31 21:10:19 +0300

    remove freemarker

--- src/main/java/com/epam/k/web/RegistrationController.java
+++ src/main/java/com/epam/k/web/RegistrationController.java
@@ -1,11 +1,11 @@
 package com.epam.k.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RegistrationController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
